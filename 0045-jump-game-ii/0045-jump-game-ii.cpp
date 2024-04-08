df6d420d44485e75f9526eb1c089440db29ee411
Junseok Oh <119016575+eDi9root@users.noreply.github.com>
2024-04-08 01:25:24 -0400

    Time: 11 ms (66%), Space: 19 MB (60.86%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0;
+        int reach = 0;
+        int end = 0;
+        for (int i = 0; i < nums.size()-1; i++) {
+            reach = max(reach, i + nums[i]);
+            if (i == end) {
+                end = reach;
+                res++;
+            }
+        }
+        return res;
+    }
+};
